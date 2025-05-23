4f1867684aa753c457bd765619dd5fc36b9e0ff6
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2025-05-23 15:15:31 +0200

    Create dossier.ps1
    Récupère les groupes de $dossier, supprime le nom de domaine dans la sortie

---
+++ dossier.ps1
@@ -0,0 +1,12 @@
+param (
+    [string]$dossier # Prend dossier en premier argument de commande
+)
+if ([string]::IsNullOrEmpty($dossier)) { # si pas d'argument de commande
+    $dossier = Read-Host "Chemin complet vers le dossier"
+}
+$dossier = $dossier.Replace('"', '') # supprime les guillemets de la variables dossier
+# Récupère les groupes de $dossier, supprime le nom de domaine dans la sortie
+((Get-Acl -Path $dossier).Access).IdentityReference -replace "^DOMAIN\\", "" | 
+Where-Object { $_ -notmatch "^(BUILTIN\\|AUTORITE NT\\)" } | # cache les groupes de BUILTIN et AUTORITE NT
+Sort-Object -Descending
+exit
