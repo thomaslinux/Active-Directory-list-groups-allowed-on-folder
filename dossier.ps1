2c66e9e2ea7b16021f8619137073389f3b0a3212
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2025-06-04 10:45:52 +0200

    Update dossier.ps1
    Only list groups

--- dossier.ps1
+++ dossier.ps1
@@ -6,33 +6,6 @@ if ([string]::IsNullOrEmpty($dossier)) { # si pas d'argument de commande
 }
 $dossier = $dossier.Replace('"', '') # supprime les guillemets de la variables dossier
 # Récupère les groupes de $dossier, supprime le nom de domaine dans la sortie
-((Get-Acl -Path $dossier).Access).IdentityReference -replace "^DOMAIN\\", "" | 
-Where-Object { $_ -notmatch "^(BUILTIN\\|AUTORITE NT\\)" } | # cache les groupes de BUILTIN et AUTORITE NT
-Sort-Object -Descending |
-Select-Object -Last 1
-
-# Créé une liste pour stocker les adresses email
-$emailArray = @()
-
-"Membres de : "
-$groups
-# Pour chaque Objet / utilisateur dans chaque groupe
-$groups | ForEach-Object {
-    Get-ADGroupMember -Identity $_ | 
-    ForEach-Object {
-        $user = Get-ADUser -Identity $_.SamAccountName -Properties Mail # Ajoute le mail de l'utilisateur dans l'Objet user
-        $email = $user.Mail
-        
-        # Ajoute les adresses email dans la liste
-        $emailArray += $email
-        
-        [PSCustomObject]@{
-            Identity = $_.Name
-            Login    = $_.SamAccountName
-            Email    = if ($email) { $email } else { "pas de mail" }
-        }
-    } | Sort-Object Login | Format-Table -AutoSize
-}
-
-# Affiche la liste des adresses email séparées par un ;
-$emailArray -join ';'
+((Get-Acl -Path $dossier).Access).IdentityReference -match "^DOMAIN\\" -replace "^DOMAIN\\", "" | 
+# Where-Object { $_ -notmatch "^(BUILTIN\\|AUTORITE NT\\)" } | # cache les groupes de BUILTIN et AUTORITE NT
+Sort-Object -Descending
